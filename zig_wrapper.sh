f61e96fea6554c06a2f965e1cf8e5d41cfbb2ad1
Homura <egzozu.be1.bas@gmail.com>
2025-04-05 22:59:47 +0300

    port this to here

--- zig_wrapper.sh
+++ zig_wrapper.sh
@@ -14,8 +14,12 @@ ld.lld | *ld.lld | ld | *-ld) exec ${ZIG_EXE} ld.lld "$@" ;;
 rc) exec $ZIG_EXE rc "$@" ;;
 strip | *-strip)
 	tmpfile="$1$(mktemp -d --dry-run .strip.XXXX)"
-	zig objcopy -S "$1" "${tmpfile}"
-	exec mv "${tmpfile}" "$1"
+	zig objcopy -S "$1" "${tmpfile}" || true
+	if [[ $? -eq 0 && -s "$tmpfile" && "$(file -b --mime-type "$tmpfile")" == "application/x-executable" ]]; then
+        	exec mv "$tmpfile" "$file"
+        else
+                exec echo "WARNING: unable to strip $file"
+        fi
 	;;
 *cc | *c++)
 	if ! test "${ZIG_TARGET+1}"; then
